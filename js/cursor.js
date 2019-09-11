5f54e62eb8971307fc353937a350376537722c5e
arisaito <saharu54@live.jp>
2019-09-12 00:01:33 +0900

    add skybox

--- js/cursor.js
+++ js/cursor.js
@@ -2,21 +2,14 @@ let start = false;
 const cursor = document.getElementById("cursor");
 const target = document.getElementById("target");
 
-let shape = [
-  '<a-icosahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" material="color: white; wireframe: true"></a-icosahedron>',
-  '<a-icosahedron color="#fee981" animation="property: rotation; to: 0 360 0; loop: false; dur: 1000; easing: easeOutCubic"></a-icosahedron>',
-  '<a-box rotation="30 30 0" color="skyblue" animation="property: rotation; to: 0 360 0; loop: false; dur: 1000; easing: easeOutCubic"></a-box>'
-];
-let lastIndex = -1;
-
-// let colors = ["pink", "lightgreen", "lightbrue"];
+let shape =
+  '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" shadow="receive: false" material="color: white; wireframe: true" animation="property: rotation; to: 0 360 0; loop: false; dur: 1000; easing: easeOutCubic"></a-dodecahedron><a-sky id="sky" radius="20" theta-length="100" material="color: black;" rotation="-90 0 0" scale="-0.5 0.5 0.5"></a-sky>';
 
 document.querySelector("#target").addEventListener("click", function(evt) {
   start = true;
   if (start) {
     console.log("click");
-    lastIndex = (lastIndex + 1) % shape.length;
-    target.innerHTML = shape[lastIndex];
+    target.innerHTML = shape;
     start = false;
   }
 });
