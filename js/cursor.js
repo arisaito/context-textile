f1e95f1d64fb0457cca47482621dbcd9046b429a
Admin <admin@ikomaharunanoMacBook-Pro.local>
2019-09-12 15:17:11 +0900

    add sceneB and C

--- js/cursor.js
+++ js/cursor.js
@@ -1,9 +1,11 @@
 let start = false;
 // const cursor = document.getElementById("cursor");
 const target = document.getElementById("target");
+var sceneB = document.getElementById("sceneB");
+var sceneC = document.getElementById("sceneC");
 
 let shape =
-  '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" shadow="receive: false" material="color: white; wireframe: true" animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic"></a-dodecahedron><a-sky id="sky" radius="20" theta-length="100" material="color: black;" rotation="-180 0 0" scale="0.01 0.01 0.01" animation="property: scale; to: 0.5 0.5 0.5; loop: false; dur: 1000;"></a-sky>';
+  '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" shader="flat" material="color: white; wireframe: true" animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic"></a-dodecahedron><a-sky id="sky" radius="20" theta-length="100" material="color: black;" rotation="-180 0 0" scale="0.01 0.01 0.01" animation="property: scale; to: 0.5 0.5 0.5; loop: false; dur: 1000;"></a-sky>';
 
 document.querySelector("#target").addEventListener("touchstart", function (evt) {
   start = true;
@@ -12,6 +14,7 @@ document.querySelector("#target").addEventListener("touchstart", function (evt) 
     target.innerHTML = shape;
     setTimeout(function scene2() {
       console.log("シーン2発火")
+
     }, 5000);
     setTimeout(function scene3() {
       console.log("シーン3発火")
