eee155521c6570c6d711525588c05ced4a82a377
Admin <admin@ikomaharunanoMacBook-Pro.local>
2019-09-12 14:02:16 +0900

    add setTimeout function

--- js/cursor.js
+++ js/cursor.js
@@ -5,11 +5,18 @@ const target = document.getElementById("target");
 let shape =
   '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" shadow="receive: false" material="color: white; wireframe: true" animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic"></a-dodecahedron><a-sky id="sky" radius="20" theta-length="100" material="color: black;" rotation="-180 0 0" scale="0.01 0.01 0.01" animation="property: scale; to: 0.5 0.5 0.5; loop: false; dur: 1000;"></a-sky>';
 
-document.querySelector("#target").addEventListener("click", function(evt) {
+document.querySelector("#target").addEventListener("touchstart", function (evt) {
   start = true;
   if (start) {
-    console.log("click");
+    console.log("touch");
     target.innerHTML = shape;
+    setTimeout(function scene2() {
+      console.log("シーン2発火")
+    }, 5000);
+    setTimeout(function scene3() {
+      console.log("シーン3発火")
+    }, 10000);
     start = false;
+    this.removeEventListener(evt);
   }
 });
