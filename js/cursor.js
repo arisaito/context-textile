b1758801e8c81445eecd4f31efa62235a5e2b6e7
arisaito <saharu54@live.jp>
2019-09-13 01:04:40 +0900

    add 3scene

--- js/cursor.js
+++ js/cursor.js
@@ -1,29 +1,68 @@
-// let start = false;
-// const cursor = document.getElementById("cursor");
+var touchArea = document.getElementById("ui");
+
 var target = document.querySelector("#target");
+var sceneA = document.getElementById("sceneA");
 var sceneB = document.getElementById("sceneB");
 var sceneC = document.getElementById("sceneC");
 
-let shape =
-  '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" shader="flat" material="color: white; wireframe: true" animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic"></a-dodecahedron><a-sky id="sky" radius="20" theta-length="100" material="color: black;" rotation="-180 0 0" scale="0.01 0.01 0.01" animation="property: scale; to: 0.5 0.5 0.5; loop: false; dur: 1000;"></a-sky>';
+var obj1 =
+  '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" shader="flat" material="color: white; wireframe: true" animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic"></a-dodecahedron>';
+var obj2 =
+  '<a-cube scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" shader="flat" material="color: white; animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic"></a-cube>';
+var obj3 =
+  '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" shader="flat" material="color: white; wireframe: true" animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic"></a-dodecahedron>';
 
-AFRAME.registerComponent("cursor-listener", {
-  init: function() {
-    target.addEventListener("click", function(evt) {
-      event.preventDefault();
-      // start = true;
-      // if (start) {
-      console.log("touch");
-      target.innerHTML = shape;
-      // setTimeout(function scene2() {
-      //   console.log("シーン2発火");
-      // }, 5000);
-      // setTimeout(function scene3() {
-      //   console.log("シーン3発火");
-      // }, 10000);
-      // start = false;
-      this.removeEventListener(evt);
-      // }
-    });
-  }
+var scene1 =
+  '<a-sky id="sky" position="0 0 0" radius="20" theta-length="100" material="color: black;" rotation="-180 0 0" scale="0.01 0.01 0.01" animation="property: scale; to: 0.5 0.5 0.5; loop: false; dur: 1000;"></a-sky>';
+var scene2 =
+  '<a-sky id="sky" position="0 0 0" radius="20" theta-length="100" material="color: pink;" rotation="-180 0 0" scale="0.01 0.01 0.01" animation="property: scale; to: 0.5 0.5 0.5; loop: false; dur: 1000;"></a-sky>';
+var scene3 =
+  '<a-sky id="sky" position="0 0 0" radius="20" theta-length="100" material="color: black;" rotation="-180 0 0" scale="0.01 0.01 0.01" animation="property: scale; to: 0.5 0.5 0.5; loop: false; dur: 1000;"></a-sky>';
+
+touchArea.addEventListener("touchstart", e => {
+  event.preventDefault();
+  target.innerHTML = obj1;
+  sceneA.innerHTML = scene1;
+
+  setTimeout(function sceneEvt2() {
+    console.log("シーン2発火");
+    target.innerHTML = obj2;
+    sceneB.innerHTML = scene2;
+  }, 5000);
+  setTimeout(function sceneEvt3() {
+    console.log("シーン3発火");
+    target.innerHTML = obj3;
+    sceneC.innerHTML = scene3;
+  }, 10000);
 });
+
+// if (flag === true) {
+//   console.log("flagがtrueになったよ");
+// }
+
+//     var marker = this.el;
+
+//     marker.addEventListener("markerFound", function() {
+//       var markerId = marker.id;
+//       console.log("marker get", markerId);
+
+//       setTimeout(function sceneAevt() {
+//         console.log("シーンA");
+//         target.innerHTML = sceneA;
+//       }, 3000);
+//     });
+//   }
+// });
+// event.preventDefault();
+// target.innerHTML = sceneA;
+// setTimeout(function scene2() {
+//   console.log("シーン2発火");
+// }, 5000);
+// setTimeout(function scene3() {
+//   console.log("シーン3発火");
+// }, 10000);
+// start = false;
+// }
+// });
+//   }
+// })
