37c9422cf40fe01e5e680ea1453dac59feb012d8
arisaito <saharu54@live.jp>
2019-09-13 01:25:28 +0900

    setting settimeout

--- js/cursor.js
+++ js/cursor.js
@@ -6,18 +6,20 @@ var sceneB = document.getElementById("sceneB");
 var sceneC = document.getElementById("sceneC");
 
 var obj1 =
-  '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" shader="flat" material="color: white; wireframe: true" animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic"></a-dodecahedron>';
+  '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" material="color" animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic"></a-dodecahedron>';
 var obj2 =
-  '<a-cube scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" shader="flat" material="color: white; animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic"></a-cube>';
+  '<a-box scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" material="color: white; animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic"></a-box>';
 var obj3 =
-  '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" shader="flat" material="color: white; wireframe: true" animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic"></a-dodecahedron>';
+  '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" shader="flat" material="color: white; wireframe: true" animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic;"></a-dodecahedron>';
+var obj4 =
+  '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" material="color: white; animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic;"></a-dodecahedron>';
 
 var scene1 =
   '<a-sky id="sky" position="0 0 0" radius="20" theta-length="100" material="color: black;" rotation="-180 0 0" scale="0.01 0.01 0.01" animation="property: scale; to: 0.5 0.5 0.5; loop: false; dur: 1000;"></a-sky>';
 var scene2 =
   '<a-sky id="sky" position="0 0 0" radius="20" theta-length="100" material="color: pink;" rotation="-180 0 0" scale="0.01 0.01 0.01" animation="property: scale; to: 0.5 0.5 0.5; loop: false; dur: 1000;"></a-sky>';
 var scene3 =
-  '<a-sky id="sky" position="0 0 0" radius="20" theta-length="100" material="color: black;" rotation="-180 0 0" scale="0.01 0.01 0.01" animation="property: scale; to: 0.5 0.5 0.5; loop: false; dur: 1000;"></a-sky>';
+  '<a-sky id="sky" position="0 0 0" radius="20" theta-length="100" material="color: black;" rotation="-180 0 0" scale="0.01 0.01 0.01" animation="property: scale; to: 0.5 0.5 0.5; loop: false; dur: 1000;" animation__2="property: position; begin: 3000; to: 0 0 3; dur: 1000; loop: false;"></a-sky>';
 
 touchArea.addEventListener("touchstart", e => {
   event.preventDefault();
@@ -28,12 +30,22 @@ touchArea.addEventListener("touchstart", e => {
     console.log("シーン2発火");
     target.innerHTML = obj2;
     sceneB.innerHTML = scene2;
-  }, 5000);
+    setTimeout(function() {
+      sceneA.style.display = "none";
+    }, 1000);
+  }, 3000);
   setTimeout(function sceneEvt3() {
     console.log("シーン3発火");
     target.innerHTML = obj3;
     sceneC.innerHTML = scene3;
-  }, 10000);
+    setTimeout(function() {
+      sceneB.style.display = "none";
+    }, 1000);
+  }, 6000);
+  setTimeout(function sceneEvt4() {
+    console.log("シーン4発火");
+    target.innerHTML = obj4;
+  }, 9000);
 });
 
 // if (flag === true) {
