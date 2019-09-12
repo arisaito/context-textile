ee1c790c5e2a4006d201de1e95ec7b4f610e7b68
arisaito <saharu54@live.jp>
2019-09-13 01:37:39 +0900

    tydy up scene

--- js/cursor.js
+++ js/cursor.js
@@ -6,20 +6,20 @@ var sceneB = document.getElementById("sceneB");
 var sceneC = document.getElementById("sceneC");
 
 var obj1 =
-  '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" material="color" animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic"></a-dodecahedron>';
+  '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" shader="flat" material="color: white; wireframe: true" animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic"></a-dodecahedron>';
 var obj2 =
-  '<a-box scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" material="color: white; animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic"></a-box>';
+  '<a-octahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" material="color: skyblue;" animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic"></a-octahedron>';
 var obj3 =
-  '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" shader="flat" material="color: white; wireframe: true" animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic;"></a-dodecahedron>';
+  '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" shader="flat" material="color: black; wireframe: true" animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic;"></a-dodecahedron>';
 var obj4 =
-  '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" material="color: white; animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic;"></a-dodecahedron>';
+  '<a-dodecahedron scale="0.6 0.6 0.6" position="0 0 0" rotation="0 0 0" material="color: black;" animation="property: rotation; to: 0 0 1000; loop: false; dur: 1000; easing: easeOutCubic;"></a-dodecahedron>';
 
 var scene1 =
-  '<a-sky id="sky" position="0 0 0" radius="20" theta-length="100" material="color: black;" rotation="-180 0 0" scale="0.01 0.01 0.01" animation="property: scale; to: 0.5 0.5 0.5; loop: false; dur: 1000;"></a-sky>';
+  '<a-sky id="sky" position="0 0 0" radius="20" theta-length="100" material="color: black;" rotation="-180 0 0" scale="0.01 0.01 0.01" animation="property: scale; to: 0.7 0.7 0.7; loop: false; dur: 1000;"></a-sky>';
 var scene2 =
-  '<a-sky id="sky" position="0 0 0" radius="20" theta-length="100" material="color: pink;" rotation="-180 0 0" scale="0.01 0.01 0.01" animation="property: scale; to: 0.5 0.5 0.5; loop: false; dur: 1000;"></a-sky>';
+  '<a-sky id="sky" position="0 0 0" radius="20" theta-length="100" material="color: pink;" rotation="-180 0 0" scale="0.01 0.01 0.01" animation="property: scale; to: 0.6 0.6 0.6; loop: false; dur: 1000;"></a-sky>';
 var scene3 =
-  '<a-sky id="sky" position="0 0 0" radius="20" theta-length="100" material="color: black;" rotation="-180 0 0" scale="0.01 0.01 0.01" animation="property: scale; to: 0.5 0.5 0.5; loop: false; dur: 1000;" animation__2="property: position; begin: 3000; to: 0 0 3; dur: 1000; loop: false;"></a-sky>';
+  '<a-sky id="sky" position="0 0 0" radius="20" theta-length="100" material="color: white;" rotation="-180 0 0" scale="0.01 0.01 0.01" animation="property: scale; to: 0.5 0.5 0.5; loop: false; dur: 1000;" animation__2="property: position; begin: 3000; to: 0 0 3; dur: 1000; loop: false;"></a-sky>';
 
 touchArea.addEventListener("touchstart", e => {
   event.preventDefault();
